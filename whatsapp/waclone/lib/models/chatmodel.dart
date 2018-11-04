2b0475f2befec136834c76079e874d4d9d5957d2
Yash Pathak <yash.pathak.5789@gmail.com>
2018-11-04 18:01:49 +0530

    Upload

---
+++ whatsapp/waclone/lib/models/chatmodel.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+      name: "Anshul",
+      message: "I lobb uhh Nunnu!",
+      time: "15:30",
+      avatarUrl:
+          "https://scontent.fluh2-1.fna.fbcdn.net/v/t1.0-0/p206x206/23517617_842179795942983_1362830308542962418_n.jpg?_nc_cat=102&_nc_ht=scontent.fluh2-1.fna&oh=320984e9a76bd84bf2cede419b2154c9&oe=5C3CA9D9"),
+  new ChatModel(
+      name: "Mumma",
+      message: "I lobb uhh Mumma!",
+      time: "16:30",
+      avatarUrl: "https://avatars0.githubusercontent.com/u/15085440?s=400&v=4"),
+  new ChatModel(
+      name: "Nunnu",
+      message: "I lobb uhh Nunnu!",
+      time: "16:39",
+      avatarUrl:
+          "https://scontent.fluh2-1.fna.fbcdn.net/v/t1.0-0/p206x206/23517617_842179795942983_1362830308542962418_n.jpg?_nc_cat=102&_nc_ht=scontent.fluh2-1.fna&oh=320984e9a76bd84bf2cede419b2154c9&oe=5C3CA9D9"),
+  new ChatModel(
+      name: "Motu",
+      message: "I lobb uhh Motu!",
+      time: "17:30",
+      avatarUrl:
+          "https://scontent.fluh2-1.fna.fbcdn.net/v/t1.0-0/p206x206/23517617_842179795942983_1362830308542962418_n.jpg?_nc_cat=102&_nc_ht=scontent.fluh2-1.fna&oh=320984e9a76bd84bf2cede419b2154c9&oe=5C3CA9D9"),
+  new ChatModel(
+      name: "BC",
+      message: "I lobb uhh Nunnu!",
+      time: "18:30",
+      avatarUrl:
+          "https://scontent.fluh2-1.fna.fbcdn.net/v/t1.0-0/p206x206/23517617_842179795942983_1362830308542962418_n.jpg?_nc_cat=102&_nc_ht=scontent.fluh2-1.fna&oh=320984e9a76bd84bf2cede419b2154c9&oe=5C3CA9D9"),
+  new ChatModel(
+      name: "Batraaa",
+      message: "I lobb uhh Batra!!",
+      time: "19:40",
+      avatarUrl:
+          "https://scontent.fluh2-1.fna.fbcdn.net/v/t1.0-0/p206x206/23517617_842179795942983_1362830308542962418_n.jpg?_nc_cat=102&_nc_ht=scontent.fluh2-1.fna&oh=320984e9a76bd84bf2cede419b2154c9&oe=5C3CA9D9"),
+  new ChatModel(
+      name: "Batraaa",
+      message: "I lobb uhh Batra!!",
+      time: "19:40",
+      avatarUrl:
+          "https://scontent.fluh2-1.fna.fbcdn.net/v/t1.0-0/p206x206/23517617_842179795942983_1362830308542962418_n.jpg?_nc_cat=102&_nc_ht=scontent.fluh2-1.fna&oh=320984e9a76bd84bf2cede419b2154c9&oe=5C3CA9D9")
+];
---
+++ whatsapp/waclone/lib/whatsapp_home.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:waclone/calls_screen.dart';
+import 'package:waclone/camera_screen.dart';
+import 'package:waclone/chat_screen.dart';
+import 'package:waclone/status_screen.dart';
+
+class WhatsAppHome extends StatefulWidget {
+  @override
+  _WhatsAppHomeState createState() => _WhatsAppHomeState();
+}
+
+class _WhatsAppHomeState extends State<WhatsAppHome>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = new TabController(vsync: this, initialIndex: 1, length: 4);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("WhatsApp"),
+        elevation: 0.7,
+        bottom: new TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            new Tab(icon: new Icon(Icons.camera_alt)),
+            new Tab(
+              text: "CHATS",
+            ),
+            new Tab(
+              text: "CALLS",
+            ),
+            new Tab(
+              text: "STATUS",
+            )
+          ],
+        ),
+        actions: <Widget>[
+          new Icon(Icons.search),
+          new Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0),
+          ),
+          new Icon(Icons.more_vert)
+        ],
+      ),
+      body: new TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          new CameraScreen(),
+          new ChatScreen(),
+          new CallScreen(),
+          new StatusScreen(),
+        ],
+      ),
+      floatingActionButton: new FloatingActionButton(
+        backgroundColor: Theme.of(context).accentColor,
+        child: new Icon(
+          Icons.message,
+          color: Colors.white,
+        ),
+        onPressed: () => print("Open Chats!"),
+      ),
+    );
+  }
+}
